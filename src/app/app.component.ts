8a563d28277f1bb0da3dc5dff9e2ce14c35c2766
Carson <c@rsonriker.com>
2019-11-09 07:15:28 -0500

    Created navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WPTS-Website';
+  links = [
+    {name: 'Listen', path: '/listen'},
+    {name: 'About', path: ''},
+    {name: 'Contact', path: ''},
+    {name: 'Music', path: ''},
+    {name: 'News', path: ''},
+    {name: 'Sports', path: ''},
+    {name: 'Podcasts', path: ''},
+    {name: 'Events', path: ''},
+  ];
 }
